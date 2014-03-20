f97248e49e370857e824d283af77f9744f2e69e3
sue7ga <sue77ga@gmail.com>
2014-03-20 15:24:20 -0700

    first commit

---
+++ address.pl
@@ -0,0 +1,40 @@
+use strict;
+use warnings;
+use utf8;
+
+use constant STATION_DATA =>
+{
+      '三重県' => {
+    'いなべ市' => {
+      'area_code' => '594',
+      'local_code_digits' => '2'
+    },
+    '三重郡' => {
+      'area_code' => '59',
+      'local_code_digits' => '3'
+    },
+  },
+  '京都府' => {
+    '与謝郡' => {
+      'area_code' => '772',
+      'local_code_digits' => '2'
+    },
+    '久世郡久御山町' => {
+      'area_code' => '75',
+      'local_code_digits' => '3'
+    },
+  },
+};
+
+sub get_station_to_pref{
+  return STATION_DATA;
+}
+
+my $data = 'aiueo';
+
+use Data::Dumper;
+
+my %data;
+%data = get_station_to_pref()->{$data};
+
+print Dumper %data;
